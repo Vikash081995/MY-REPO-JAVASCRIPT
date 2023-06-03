213ee0bc91f2ccf2635ec57342d3337bccad2153
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2023-06-04 01:29:15 +0530

    spleeling changed

--- Basic/typeconversion.js
+++ Basic/typeconversion.js
@@ -1,14 +1,14 @@
-//string to Number 
+// The simplest way to perform an explicit type conversion is to use the Boolean(), Number(), and String() functions:
+//============================================
+//string to Number :Number() method
 Number("3.14");  //3.14
 Number("");  //0
 Number("99 99") //NaN
 let y="5"
 let x=+y;
 console.log(x) //5
-
-
-//Number to string 
-//String()method
+//============================================
+//Number to string : String() method ,toString Method
 console.log(String(123));
-//toString Method
 (123).toString() //"123"
+//============================================
