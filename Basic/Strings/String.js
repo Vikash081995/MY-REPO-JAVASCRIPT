c738f0b9a1f6e942dfe357fbe5e6a9fe1d514e1a
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2023-06-13 16:20:52 +0530

    strings modified

--- Basic/Strings/String.js
+++ Basic/Strings/String.js
@@ -1,36 +1,45 @@
-let fname = "abcdefghijkl";
-console.log(fname.length);
-console.log(fname[fname.length - 2]);
+//strings are enclosed within either single qutes ,double quotes or backticks.
+let str24 = "Hello";
+let str2 = "Single Quote string";
 
+let single = "singlequote";
+let backtick = `stringwithbacktick`;
+//==================================================
+(""); // The empty string: it has zero characters
+("testing");
+("3.14");
+('name="myform"');
+("Wouldn't you prefer O'Reilly's book?");
+("τ is the ratio of a circle's circumference to its radius");
+
+// A string representing 2 lines written on one line:
+("two\nlines");
+
+// A one-line string written on 3 lines:
+"one\
+ long\
+ line" // A two-line string written on two lines:
+`the newline character at the end of this line
+is included literally in this string`;
+//=================================================
+// Accessing Characters : use charAt(position)
+//=================================================
+let strHello = "Hello";
+console.log(strHello.charAt(0)); //H
+//=================================================
+// difference between Square Brackets and charAt
+//=================================================
+//if no character is found , [ ] return undfined , charAt return empty string ("").
+let strName ="Hello";
+console.log(strName[10]); //undefined
+console.log(strName.charAt(10)); // ""
 // ======================================
-let text = " peter Jordanssssa";
-let result = text.length;
-console.log(text.length);
-console.log(text.toLocaleLowerCase()); // peter jordanssssa
-console.log(text.toUpperCase()); // PETER JORDANSSSSA
-console.log(text.charAt(0));
-console.log(text.charAt(text.length - 1)); //a
-console.log(text.indexOf("e")); //2
-console.log(text); //peter Jordanssssa
-console.log(text.trim()); //peter Jordanssssa
-console.log(text.trim().toLowerCase().startsWith("peter")); //true
-console.log(text.includes("eter")); //true
-console.log(text.slice(0, 2)); //p
-console.log(text.slice(-3)); //ssa
-// ============================================
 // string  concatenation
 // ============================================
 let string1 = "17";
 let string2 = "10";
 
 let newString1 = +string1 + +string2;
-console.log(typeof newString1);
+console.log(typeof newString1); //number
+console.log(newString1); //27
 //=============================================
-//to insert quotes in string use backslash '\'
-console.log("i'm the walrus");//i'm the walrus
-//=============================================
-//searching for a substring 
-//=============================================
-let str='widget with id';
-console.log(str.indexOf('widget')); //0
-console.log(str.indexOf('id')); //1
---
+++ main.js
@@ -0,0 +1,3 @@
+let strName ="Hello";
+console.log(strName[10]); //undefined
+console.log(strName.charAt(10)); 
